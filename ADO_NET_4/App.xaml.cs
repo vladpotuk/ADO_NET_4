e03cea0339c9d00641b16281e777ccc7df5b6c26
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2024-05-14 18:54:55 +0300

    Add project files.

---
+++ ADO_NET_4/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ADO_NET_4
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ ADO_NET_4/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO_NET_4
+{
+    internal class Class1
+    {
+    }
+}
